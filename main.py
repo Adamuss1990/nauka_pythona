a9551034e234b7a822a2bc81efefab01de2ffc76
Adamuss1990 <Adamuss1990@users.noreply.github.com>
2018-10-07 13:31:11 +0200

    main.py

---
+++ main.py
@@ -0,0 +1,36 @@
+#marka = ' polonez'
+#ilosc_drzwi = 5
+#print ("Nowa marka to " + marka + ' z ' +  str( ilosc_drzwi ) + '!')
+#print(marka.upper())
+#print(marka.lower())
+
+
+#rachunek = 300
+
+
+#print ("W styczniu rachunek za gaz wynosi" + str( rachunek) )
+#print ("ile miesiecy)")
+#print ("miesiecy bedzie kosztowac)" + str(rachunek* input([])))
+
+
+#szkola = ['podstawowa','srednia']
+#klasa = [1,3]
+
+#len okresla nam dlugosc lini
+#print(len(ilosc))
+
+
+#for s in szkola:
+#    print(s)
+#for k in klasa:
+#    print(k)
+
+#aby wypisac obok siebie wartosci
+szkola = ['podstawowa', 'srednia']
+klasa = [1, 3]
+
+print(" dlugosc: {0}".format(len(szkola)))
+
+for idx in range(len(szkola) ) :
+    print( " idx: " + str(idx) + ": " + szkola[idx])
+    print(szkola[idx] + " ilosc klas " + str(klasa[idx]))
